4eb446627a8dc0ade80f46b9066dafcb2098fd31
santos22 <santosjavier22@gmail.com>
2015-10-10 00:42:05 -0500

    Added blurb and image property and modified constructor

--- ListsAndCards/app/src/main/java/com/hathy/listsandcards/Person.java
+++ ListsAndCards/app/src/main/java/com/hathy/listsandcards/Person.java
@@ -3,11 +3,15 @@ package com.hathy.listsandcards;
 class Person {
     String name;
     String age;
+    String blurb;
+    String image;
     int photoId;
 
-    Person(String name, String age, int photoId) {
+    Person(String name, String age, String blurb, String image, int photoId) {
         this.name = name;
         this.age = age;
+        this.blurb = blurb;
+        this.image = image;
         this.photoId = photoId;
     }
 }
